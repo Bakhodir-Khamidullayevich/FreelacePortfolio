e312c9001b6f8c3d6e63bf5232264a9f89cad2a1
Bakhodir-Khamidullayevich <157077959+Bakhodir-Khamidullayevich@users.noreply.github.com>
2024-01-21 00:25:23 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const btnDarkMode = document.querySelector('.dark-mode-btn');
+
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+	btnDarkMode.classList.add('dark-mode-btn--active');
+	document.body.classList.add('dark');
+}
+
+if (localStorage.getItem('darkMade') === 'dark') {
+	btnDarkMode.classList.add('dark-mode-btn--active');
+	document.body.classList.add('dark');
+} else if(localStorage.getItem('darkMade') === 'light'){
+	btnDarkMode.classList.remove('dark-mode-btn--active');
+	document.body.classList.remove('dark');
+};
+
+window.matchMedia&& window.matchMedia("(prefers-color-scheme: dark)").addEventListener('change', (event)=>{
+	const newColorScheme = event.matches ? 'dark' : 'light';
+
+	if (newColorScheme === 'dark') {
+		btnDarkMode.classList.add('dark-mode-btn--active');
+		document.body.classList.add('dark');
+		localStorage.setItem('darkMade', 'dark')
+	} else {
+		btnDarkMode.classList.remove('dark-mode-btn--active');
+		document.body.classList.remove('dark');
+	}
+})
+btnDarkMode.onclick = function () {
+	btnDarkMode.classList.toggle('dark-mode-btn--active');
+	const isDark = document.body.classList.toggle('dark');
+
+	if (isDark) {
+		localStorage.setItem('darkMade', 'dark');
+	} else {
+		localStorage.setItem('darkMade', 'light');
+	}
+}
